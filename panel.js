c38ae28e7df907830e1a1f26fddc5951d77518b4
Michal Kuklis <michal.kuklis@gmail.com>
2013-01-31 02:00:14 -0500

    more refactoring

--- panel.js
+++ panel.js
@@ -1,32 +1,89 @@
+/**
+ * panel.js v1.0.0
+ * Licensed under The MIT License
+ * http://opensource.org/licenses/MIT
+ */
+
 (function () {
 
-  var positions = {
-    left: 'panel-transition-left',
-    right: 'panel-transition-right'
+  var positionMap = {
+    left:  'panel-left',
+    right: 'panel-right'
   };
 
-  var pushRevealRegex = /reveal|push/;
+  var transitionEnd = "webkitTransitionEnd";
 
-  var open = function (panel, transition) {
-    var method = (transition == "reveal") ? 'remove' : 'add';
+  var open = {
+    overlay: function (panel, pos) {
+      addStyles(panel, pos, -panel.offsetWidth + 'px');
 
-    panel.classList[method](positions.left);
+      setTimeout(function () {
+        addStyles(panel, 'opacity', 1, 'z-index', 999, pos, '0px');
+        panel.classList.add(positionMap[pos]);
+      });
+    },
 
-    if (transition.match(pushRevealRegex) != null) {
-      getSiblings(panel).forEach(function (sibling) {
-        if (!sibling.classList.contains('panel')) {
-          sibling.classList.add(positions.left);
-          sibling.style.left = panel.offsetWidth + 'px';
-        }
+    reveal: function (panel, pos) {
+      addStyles(panel, 'opacity', 1, 'z-index', 0, pos, '0px');
+
+      findSiblings(panel).forEach(function (sibling) {
+        sibling.classList.add(positionMap[pos]);
+        addStyles(sibling, pos, panel.offsetWidth + 'px');
+      });
+    },
+
+    push: function (panel, pos) {
+      addStyles(panel, 'opacity', 1, 'z-index', 0, pos, '0px');
+      panel.classList.add(positionMap[pos]);
+
+      findSiblings(panel).forEach(function (sibling) {
+        sibling.classList.add(positionMap[pos]);
+        addStyles(sibling, pos, panel.offsetWidth + 'px');
       });
     }
+  };
 
-    panel.classList.add(transition);
+  var close = {
+    overlay: function (panel, pos) {
+      panel.addEventListener(transitionEnd, clean);
+      addStyles(panel, pos, -panel.offsetWidth + 'px');
+    },
+
+    reveal: function (panel, pos) {
+      findSiblings(panel).forEach(function (sibling) {
+        sibling.panel = panel;
+        removeStyles(sibling, pos);
+        sibling.addEventListener(transitionEnd, clean);
+      });
+    },
+
+    push: function (panel, pos) {
+      findSiblings(panel).forEach(function (sibling) {
+        removeStyles(sibling, pos);
+        sibling.addEventListener(transitionEnd, clean);
+      });
+
+      panel.addEventListener(transitionEnd, clean);
+      addStyles(panel, pos, -panel.offsetWidth + 'px');
+    }
+  };
+
+  var clean = function (e) {
+    var node = e.target;
+
+    node.removeEventListener(transitionEnd, clean);
+    node.classList.remove(positionMap[e.propertyName]);
+    removeStyles(node, e.propertyName, "opacity", "z-index");
+
+    if (node.panel) {
+      removeStyles(node.panel, e.propertyName, "opacity", "z-index");
+      delete node.panel
+    }
   }
 
   var closeAll = function (e) {
+    var panel, panels, position;
     var i = 0;
-    var panel, panels;
 
     if (e.target.classList.contains('panel') ||
         hasParent(e.target, 'panel')) return;
@@ -34,21 +91,11 @@
     panels = document.querySelectorAll('.panel');
 
     while (panel = panels[i++]) {
-      close(panel, panel.dataset.transition);
+      position = panel.dataset.position || "left";
+      transition = panel.dataset.transition || "overlay";
+      close[transition](panel, position);
     }
   };
-
-  var close = function (panel, className) {
-    getSiblings(panel).forEach(function (sibling) {
-      if (!sibling.classList.contains('panel')) {
-        sibling.style.left = '0px';
-      }
-    });
-
-    if (className != "reveal") {
-      panel.classList.remove(className);
-    }
-  }
 
   var getPanel = function (e) {
     var anchor = e.target;
@@ -71,13 +118,15 @@
     return false;
   };
 
-  var getSiblings = function (node, callback) {
+  var findSiblings = function (node) {
     var child = node.parentNode.firstChild;
     var siblings = [];
 
     while (child = child.nextSibling) {
       if (child.nodeType == 1 &&
-          node != child) {
+          node != child &&
+          !child.classList.contains('panel')) {
+
         siblings.push(child);
       }
     }
@@ -85,10 +134,35 @@
     return siblings;
   };
 
+  var addStyles = function (node, styles) {
+    var args, i, key;
+
+    if (styles === Object(styles)) {
+      for (key in styles) {
+        node.style[key] = styles[key];
+      }
+    }
+    else {
+      args = arguments;
+      for (i = 1, l = args.length - 1; i < l; i += 2) {
+        node.style[args[i]] = args[i + 1];
+      }
+    }
+  };
+
+  var removeStyles = function (node, styles) {
+    var i;
+    var args = arguments;
+
+    for (i = 1, l = args.length; i < l; i++) {
+      node.style[args[i]] = "";
+    }
+  };
+
   // events
 
   window.addEventListener('click', function (e) {
-    var action, transition, state;
+    var action, transition, state, position;
     var panel = getPanel(e);
 
     if (!panel) {
@@ -100,15 +174,18 @@
     action = e.target.dataset.action || "open";
     transition = panel.dataset.transition || "overlay";
     state = panel.dataset.state || "open";
+    position = panel.dataset.position || "left";
 
     if (action == "open" && state == "open") {
-      open(panel, transition);
-      panel.dataset.state  = "close";
+      open[transition](panel, position);
+      panel.dataset.state = "close";
     }
     else {
-      close(panel, transition);
-      panel.dataset.state  = "open";
+      close[transition](panel, position);
+      delete panel.dataset.state;
     }
+
+    e.preventDefault();
 
   }, false);
 
